5fd552905b5198b1055753b87df9d4abe85cddd3
Donnie Mattingly <donniewmattingly@gmail.com>
2020-11-23 15:54:28 -0500

    update firestore rules

--- src/hooks.js
+++ src/hooks.js
@@ -5,10 +5,13 @@ import {useStoreActions} from "easy-peasy";
 
 export const useFirebaseAuth = () => {
     const setAuth = useStoreActions(actions => actions.setAuth);
+    const loadUser = useStoreActions(actions => actions.loadUser);
+
     useEffect(() => {
         const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
             if (user) {
                 setAuth(user);
+                loadUser(user.uid);
             }
         });
         return () => unsubscribe();
--- src/store.js
+++ src/store.js
@@ -4,25 +4,29 @@ import firebase from "firebase";
 export const store = createStore({
     auth: {loggedIn: false},
     loggedIn: computed(state => state.auth.loggedIn),
-    setAuth: action((state, user) => {
-        if (user) {
-            return {
-                ...state,
-                auth: {
-                    loggedIn: true,
-                    name: user.displayName,
-                    id: user.uid,
-                }
+    setAuth: action((state, firebaseUser) => {
+        console.log(firebaseUser);
+        if (firebaseUser) {
+            state.auth = {
+                loggedIn: true,
+                name: firebaseUser.displayName,
+                id: firebaseUser.uid,
             }
         } else {
-            return {
-                ...state,
-                auth: {loggedIn: false}
-            }
+            state.auth = {loggedIn: false}
+        }
+    }),
+    user: {},
+    setUser: action((state, user) => state.user = user),
+    loadUser: thunk(async (actions, id) => {
+        const doc = await firebase.firestore().collection('users').doc(id).get();
+        console.log(doc);
+        if (doc.exists) {
+            console.log(doc.data());
+            actions.setUser(doc.data());
         }
     }),
     signOut: thunk(async (actions, payload) => {
-        console.log('signing out');
         await firebase.auth().signOut();
         actions.setAuth(null);
     })
